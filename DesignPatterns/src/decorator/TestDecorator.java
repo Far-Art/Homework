49f11b40440864dc832ffc14319e1a2b360c9691
Artur Farmanov <arturfarmanov91@gmail.com>
2021-03-30 23:13:52 +0300

    set component interface instead of hardware class as instance

--- DesignPatterns/src/decorator/TestDecorator.java
+++ DesignPatterns/src/decorator/TestDecorator.java
@@ -1,6 +1,6 @@
 package decorator;
 
-import decorator.classes.Hardware;
+import decorator.classes.Component;
 import decorator.classes.Motherboard;
 import decorator.classes.Proccessor;
 import decorator.classes.Pwm;
@@ -10,8 +10,8 @@ import decorator.classes.VideoCard;
 public class TestDecorator {
 
 	public static void main(String[] args) {
-		Hardware hardware = new Motherboard(new VideoCard(new SoundCard(new Pwm(new Proccessor()))));
-		System.out.println(hardware.getComponents());
+		Component component = new Motherboard(new VideoCard(new SoundCard(new Pwm(new Proccessor()))));
+		System.out.println(component.getComponents());
 	}
 
 }
